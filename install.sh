3345ea0a9dae6fb2c628d0a6ed6249771a811e14
Ivan Shapovalov <intelfx100@gmail.com>
2013-08-08 18:47:13 +0400

    Fix colorscheme install.

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 kde_path="$(kde4-config --localprefix)"
 
 # KDE Color Palette
-install -Dm644 Solarized_Colors "${kde_path}/share/config/colors"
+install -Dm644 Solarized_Colors "${kde_path}/share/config/colors/Solarized"
 
 # Kate Color Schema and syntax highligting
 for file in *Dark*; do
